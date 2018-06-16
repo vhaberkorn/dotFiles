511589690493ace881bc5a2aba76c3f3075b4d93
Victor Haberkorn Gomes <vhaberkorn@gmail.com>
2018-06-16 17:37:29 -0300

    adding zsh aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,9 +1,14 @@
+# *NIX
 alias du='du -kh --max-depth=1'
 alias df='df -h' 
 alias cls='clear'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+
+# ZSH
+alias zshconfig="vim ~/.zshrc"
+alias reloadzsh="source ~/.zshrc && echo 'Zsh Reloaded!'"
 
 # Docker
 alias stop_and_kill_dockers='sudo docker stop $(sudo docker ps -aq) && sudo docker rm $(sudo docker ps -aq)'
