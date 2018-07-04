2766d74caf5a678afecb3f2f8cc123863e40e9e5
Victor Haberkorn Gomes <vhaberkorn@gmail.com>
2018-07-04 11:12:15 -0300

    addin gpg_help

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -10,9 +10,11 @@ alias egrep='egrep --color=auto'
 alias zshconfig="vim ~/.zshrc"
 alias reloadzsh="source ~/.zshrc && echo 'Zsh Reloaded!'"
 
+alias gpg_help="echo 'Create keys:   		gpg --full-gen-key\nExport public key:	gpg --export  -a <user-id>\nImport public key:	gpg --import <pub.key>\nEncryption: 		gpg -r <user-id> -e <file>\nDecryption:		gpg -d <file.gpg>\nSign: 			gpg -s <file>\nVerify:			gpg --verify <file.sig>\n'"
+
+
 # Docker
 alias stop_and_kill_dockers='sudo docker stop $(sudo docker ps -aq) && sudo docker rm $(sudo docker ps -aq)'
 
 # OpenVPN
-alias start_pia='sudo cd /etc/openvpn/client/normal/; sudo openvpn Brazil.ovpn'
-
+alias start_pia='sudo cd /etc/openvpn/client; sudo openvpn Brazil.ovpn'
